e32026ad8e8d1d20c7a166b39fcc4900ff6d3b38
Christoffer Hafsahl <chris@bjelleklang.org>
2016-12-11 23:17:27 +0100

    Initial commit

---
+++ app/src/main/java/no/chrish/lm/livemetronome/Metronome.java
@@ -0,0 +1,13 @@
+package no.chrish.lm.livemetronome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Metronome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metronomelayout);
+    }
+}
