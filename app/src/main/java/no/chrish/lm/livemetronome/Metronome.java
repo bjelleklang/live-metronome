ea3aaab60ef3e5718c9abe1bb01d41cdd9a72744
Christoffer Hafsahl <chris@bjelleklang.org>
2016-12-13 09:58:48 +0100

    Minor stuff

--- app/src/main/java/no/chrish/lm/livemetronome/Metronome.java
+++ app/src/main/java/no/chrish/lm/livemetronome/Metronome.java
@@ -2,12 +2,18 @@ package no.chrish.lm.livemetronome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class Metronome extends AppCompatActivity {
+    SeekBar tempoSlider = (SeekBar) findViewById(R.id.temposlider);
+    int tempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.metronomelayout);
+
+
     }
+
 }
